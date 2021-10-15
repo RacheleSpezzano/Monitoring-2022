2f229c6a37ebee81a66867e3b171b6ddf59eaebc
Rachele Spezzano <92572872+RacheleSpezzano@users.noreply.github.com>
2021-10-15 11:00:37 +0200

    Create R_first_code.r

---
+++ R_first_code.r
@@ -0,0 +1,12 @@
+#This is my first code in GitHub! Quite exciting right?
+
+# Here are the imput data 
+# Costanza data on streams
+water <- c(100, 200, 300, 400, 500)
+
+#Marta data on fish genomes
+fish <- c(10, 50, 60, 100, 200)
+
+# plot the diverty of fish (y) versus the amount of water (x)
+plot(water, fish)
+
